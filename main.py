25c9c1b5635601dcef694dbb97af7520d6df7591
Thejan K.G.T <99307599+IT21222740@users.noreply.github.com>
2025-04-20 11:28:25 +0530

    implement firebase config

--- main.py
+++ main.py
@@ -20,5 +20,21 @@ if not firebase_admin._apps:
     cred = credentials.Certificate("ecommerce-microservices.json")
     firebase_admin.initialize_app(cred)
 
+# Firebase Configuration
+firebaseConfig = {
+  "apiKey": "AIzaSyCkeYLF7sUEHQdURFZm-bRvAs5jL7u5faE",
+  "authDomain": "ecommerce-microservices-93118.firebaseapp.com",
+  "projectId": "ecommerce-microservices-93118",
+  "storageBucket": "ecommerce-microservices-93118.firebasestorage.app",
+  "messagingSenderId": "778737608790",
+  "appId": "1:778737608790:web:aec96b98f4d70b1453f3f7",
+  "measurementId": "G-541YPPWKZE",
+  "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app",reload=True)
