29a6aac64861d580b21e52456b2ec86c57f1ba2b
IT21268908 <it21268908@my.sliit.lk>
2025-05-10 11:46:22 +0530

    Add SignInSchema and SignUpSchema models for user authentication

---
+++ Models/SignIn.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class SignInSchema(BaseModel):
+    email: str
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "sample@gmail.com",
+                "password": "samplepass123"
+            }
+        }
---
+++ Models/SignUp.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+class SignUpSchema(BaseModel):
+    email: str
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "sample@gmail.com",
+                "password": "samplepass123"
+            }
+        }
